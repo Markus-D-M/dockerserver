94ef376537555f31f53329609ba99ea1a9967119
Markus-D-M <mar.d@gmx.net>
2023-12-03 08:27:01 +0100

    nextcloud

--- init/init.sh
+++ init/init.sh
@@ -10,15 +10,29 @@ else
 fi
 
 if [ ! -f /root_env/mysql/secrets/mysql_postdove ]; then
-  /bin/dd bs=32 count=1 if=/dev/urandom 2> /dev/null | /bin/base64 > /root_env/mysql/secrets/mysql_postdove
+  /bin/dd bs=32 count=1 if=/dev/urandom 2> /dev/null | /bin/base64 > /root_env/mysql/secrets/mysql_postdove_password
   echo "[X] creating new mysql postdove password"
 else
   echo "[ ] creating new mysql postdove password"
 fi
 
+if [ ! -f /root_env/mysql/secrets/mysql_nextcloud ]; then
+  /bin/dd bs=32 count=1 if=/dev/urandom 2> /dev/null | /bin/base64 > /root_env/mysql/secrets/mysql_nextcloud_password
+  echo "[X] creating new mysql nextcloud password"
+else
+  echo "[ ] creating new mysql nextcloud password"
+fi
+
+
 # amavis
 /bin/mkdir -p /root_env/amavis/quarantine
 
+# nextcloud
+/bin/mkdir -p /root_env/nextcloud/data
+/bin/mkdir -p /root_env/nextcloud/secrets
+cp /root_env/mysql/secrets/mysql_nextcloud_password /root_env/nextcloud/secrets/
+echo "nextcloud" > /root_env/nextcloud/secrets/mysql_nextcloud_user
+echo "nextcloud" > /root_env/nextcloud/secrets/mysql_nextcloud_database
 
 
 
